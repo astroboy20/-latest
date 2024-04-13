2554801c996a3824eceef6e892f5bbfaab269911
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-04-13 16:53:50 +0100

    feat: fetched data with react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  // const fetchData =async ()=>{
+  //   const res =  await
 
+  //   return res.json()
+  //   console.log(res.json())
+  // }
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: async () => {
+      // Make the query function async to use await
+      try {
+        const res = await fetch("https://api.github.com/repos/TanStack/query");
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return res.json(); // Return the JSON parsed response
+      } catch (error) {
+        throw new Error("Error fetching data from API: " + error.message);
+      }
+    },
+  });
+
+  if (isError) return <div>Error: {isError.message} </div>;
+  if (isLoading) return <div>Loading ...</div>;
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{data?.name}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
